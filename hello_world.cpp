3333a898347f23aa66e4fce5167284fd79584dc6
nabeelali1207 <nabeel1207ali@gmail.com>
2023-09-12 14:17:12 +0500

    Adding new function to display name #1

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main()
 {
+	string name = "Fatimah";
 	cout << "Hello, World!" << endl;
 	return 0;
-	cout << "My name is Fatimah." << endl;
+	cout << "My name is " << name << "." << endl;
 }
